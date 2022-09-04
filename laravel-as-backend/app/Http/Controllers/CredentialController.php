a0727b87a3f0ce691cc6c9a3b6389ccbee3085e0
andydevs69420 <redondophilippandrew@gmail.com>
2022-09-04 09:02:33 +0800

    added more catch clausess

--- laravel-as-backend/app/Http/Controllers/CredentialController.php
+++ laravel-as-backend/app/Http/Controllers/CredentialController.php
@@ -20,26 +20,25 @@ class CredentialController extends Controller
         $password = $request->input("password");
         $selected_plan = $request->input("plan");
 
-        $message_result = ([
-            "status"   => "",
-            "email"    => "",
-            "password" => "",
+        $response = ([
+            "status"  => "",
+            "message" => "",
         ]);
 
         /** ensure email does not exist! */
         if (User::emailExist($request->input("email")))
         {
-            $message_result["status"] = "bad";
-            $message_result["email"]  = "This email is alread used!";
-            return json_encode($message_result);
+            $response["status" ] = "bad";
+            $response["message"] = "This email is alread used!";
+            return json_encode($response);
         }
 
         /** insert user first */
         $user = User::initialSave($email, $password);
 
-        $message_result["status"] = "ok";
-        $message_result["email" ] = "Successfully signedup!";
-        return json_encode($message_result);
+        $response["status" ] = "ok";
+        $response["message"] = "Successfully signedup!";
+        return json_encode($response);
     }
 
     /**
@@ -53,8 +52,19 @@ class CredentialController extends Controller
         $email = $request->input("email");
         $password = $request->input("password");
 
+        $response = ([
+            "status"  => "",
+            "message" => "",
+        ]);
+
         /** retrieve user by email */
         $user = User::getByEmail($email);
+
+        if (!$user)
+        {   // user not exist
+
+            return;
+        }
 
         error_log($user);
     }
